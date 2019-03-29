9e3e92e7e0ee2a136cbee653102ef740ea974bba
4512 Otter Chaos <chsbearbots@gmail.com>
2019-03-28 17:34:32 -0700

    woops caps

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,14 +10,16 @@ package frc.robot;
 import frc.robot.common.MConstants;
 
 public class RobotMap {
+    //drivebase
     public static final double WHEEL_DIAMETER = 8;//inches
     public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER*Math.PI;
     public static final double INCHES_PER_COUNT = WHEEL_CIRCUMFERENCE / MConstants.kRotCounts;
     public static final double COUNTS_PER_100MS_TO_INCHES_PER_SEC = INCHES_PER_COUNT * 10;//100ms is 1/10th of 1 second
-    public static final double frontLimeHeight=38.5;//all inches
-    public static final double backLimeHeight=0;
-    public static final double frontLimeAngle=11;
-    public static final double backLimeAngle=0;
-    public static final double tapeHeight=28.5;
-    public static final double tapeDistanceFlat=0;
+    //limelight
+    public static final double FRONTLIME_HEIGHT=38.5;//all inches
+    public static final double BACKLIME_HEIGHT=0;
+    public static final double FRONTLIME_ANGLE=11;
+    public static final double BACKLIME_ANGLE=0;
+    public static final double TAPE_HEIGHT=28.5;
+    public static final double TAPE_DISTANCE=0;
 }
